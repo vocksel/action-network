808ef12a5acb2a9a40d247fba7a39631d0954b9e
David Minnerly <voxeldavid@gmail.com>
2018-10-13 17:05:38 -0400

    Spruce up assertions

--- lib/init.lua
+++ lib/init.lua
@@ -7,6 +7,10 @@ local network = {
     _actions = {}
 }
 
+local CAN_ONLY_CALL_FROM_SERVER = "%s() can only be called from the server"
+local CAN_ONLY_CALL_FROM_CLIENT = "%s() can only be called from a client"
+local BAD_ARGUMENT = "bad argument #%i to '%s' (%s expected, got %s)"
+
 local function createRemote()
     local remote = Instance.new("RemoteEvent")
     remote.Name = network.REMOTE_NAME
@@ -23,7 +27,7 @@ end
 
 -- server only
 function network.init()
-    assert(runService:IsServer(), "network.init() can only be called from the server")
+    assert(runService:IsServer(), CAN_ONLY_CALL_FROM_SERVER:format("network.init"))
 
     local remote = createRemote()
 
@@ -40,17 +44,16 @@ end
 
 -- server only
 function network.on(actionType, callback)
-    assert(runService:IsServer(), "'network.on()' can only be called from the server")
-    assert(type(actionType) == "string", ("bad argument #1 to 'network.on()' (string expected, got %s)"):format(type(actionType)))
-    assert(type(callback) == "function", ("bad argument #2 to 'network.on()' (function expected, got %s"):format(type(callback)))
+    assert(type(actionType) == "string", BAD_ARGUMENT:format(1, "network.on()", "string", type(actionType)))
+    assert(type(callback) == "function", BAD_ARGUMENT:format(2, "network.on()", "function", type(callback)))
 
     network._actions[actionType] = callback
 end
 
 -- client only
 function network.dispatch(action)
-    assert(runService:IsClient(), "'network.dispatch()' can only be called from a client")
-    assert(type(action) == "table", ("bad argument #1 to 'network.dispatch() '(table expected, got %s)"):format(type(action)))
+    assert(runService:IsClient(), CAN_ONLY_CALL_FROM_CLIENT:format("network.init"))
+    assert(type(action) == "string", BAD_ARGUMENT:format(1, "network.dispatch()", "table", type(action)))
 
     local remote = clientGetRemote()
     remote:FireServer(action)
